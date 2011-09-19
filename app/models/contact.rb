291e1d4a05a3fed287c0592344b568a5443fccd8
Luis Perichon <luisperichon+github@gmail.com>
2011-09-19 13:05:26 -0300

    dont validate presence of last name

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -9,7 +9,6 @@ class Contact
   field :last_name
 
   validates :first_name, :presence => true
-  validates :last_name, :presence => true
 
   def full_name
     "#{first_name} #{last_name}"
--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -5,5 +5,5 @@ describe Contact do
 
   it { should have_fields :first_name, :last_name }
 
-  it { should validate_presence_of :last_name }
+  it { should validate_presence_of :first_name }
 end
