5804c27689e37ff7f1aff8d3647f9c93b240f5ac
Luis Perichon <luisperichon+github@gmail.com>
2011-10-21 13:38:30 -0300

    Identification unique validations

--- app/models/identification.rb
+++ app/models/identification.rb
@@ -1,7 +1,19 @@
 class Identification < ContactAttribute
-  field :name
+  field :category
   field :value
 
-  validates :value, :presence => true # , :uniqueness => {:scope => :name}
-  validates :name, :presence => true #, :uniqueness => {:scope => :contact}
+  validates :value, :presence => true, :uniqueness => {:scope => :category}
+  validates :category, :presence => true, :uniqueness => {:scope => :contact}
+
+  before_validation :check_value_uniqueness
+
+  def check_value_uniqueness
+    return if _parent.nil?
+    if Contact.where({'_id' => {'$ne' =>contact._id}}).and({'contact_attributes.value' => value}).and({'contact_attributes.category' => category}).exists?
+      errors.add(
+            :value,
+            :taken
+          )
+    end
+  end
 end
