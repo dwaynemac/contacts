71886c4bfea92484a45c9dc153d5b3aab3c8c5a0
Luis Perichon <luisperichon+github@gmail.com>
2011-10-25 17:13:17 -0300

    fix email uniqueness validator

--- app/validators/email_uniqueness_validator.rb
+++ app/validators/email_uniqueness_validator.rb
@@ -1,7 +1,7 @@
 require 'mail'
 class EmailUniquenessValidator < ActiveModel::EachValidator
   def validate_each(record,attribute,value)
-    r = Contact.any_of({'contact_attributes._type' => 'Email', 'contact_attributes.value' => value})
+    r = Contact.any_of({'_id' => {'$ne' =>record.contact._id}, 'contact_attributes._type' => 'Email', 'contact_attributes.value' => value})
 
     record.errors[attribute] << (options[:message] || "is not unique") if r.count > 0
   end
