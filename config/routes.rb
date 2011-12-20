34778a6d198dcada3bedcd1fde230f8e5cb4a764
Luis Perichon <luisperichon+github@gmail.com>
2011-12-20 11:15:46 -0300

    remove contact_duplications controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,9 @@ Contacts::Application.routes.draw do
   namespace 'v0' do
     resources :contacts do
       resource :avatar, :only => [:create, :destroy]
-      resources :contact_duplications, :only => :index
     end
     resources :contact_attributes
     resource :avatar, :only => [:create, :destroy]
-    resources :contact_duplications, :only => :index
     scope "/accounts/:account_name" do
       resources :contacts do
         resources :contact_attributes
