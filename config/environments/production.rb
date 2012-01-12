a6f2f3d0750bd876eb3b2dda10873d518d344aff
Luis Perichon <luisperichon+github@gmail.com>
2012-01-11 21:17:40 -0300

    disable mongoid logs on production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,4 +57,7 @@ Contacts::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+
+  #Disable mongoid logs
+  Mongoid.logger = nil
 end
