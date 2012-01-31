0d3558ae86c1794732c52e80da1dad841543e3a3
Alex Falke <afalkear@gmail.com>
2012-01-31 15:48:56 -0300

    carrierwave bug on heroku fix

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -16,8 +16,8 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "images/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-    #"#{Rails.root}/spec/support/image_DB"#images/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    #"images/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "#{Rails.root}/tmp/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
