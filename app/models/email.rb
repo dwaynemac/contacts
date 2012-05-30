ac5d03535d7783f1886f1a04402100628cc1d477
sebareca <sebareca@gmail.com>
2012-05-30 17:58:05 -0300

    [#30403847] Normalizing email before saving

--- app/models/email.rb
+++ app/models/email.rb
@@ -1,5 +1,13 @@
 class Email < ContactAttribute
+
+  before_save :normalize_email
+
   field :category
 
   validates :value, :email_format => true, :email_uniqueness => true
+
+  private
+  def normalize_email
+    value.downcase!
+  end
 end
