535ef6c96e004497f7407b4103932b0fb038aaf7
Dwayne Macgowan <dwaynemac@gmail.com>
2012-07-13 17:42:26 -0300

    [#31235887] Remove doc-comments from uploader, they're already in AvatarUploader

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -2,37 +2,10 @@
 
 class ImageUploader < CarrierWave::Uploader::Base
 
-  # Include RMagick or ImageScience support:
   # RMagick needs imagemagick installed on the computer, almost always available in Unix systems
   # It also needs this package: "sudo apt-get install libmagick9-dev"
   include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
-  # include CarrierWave::ImageScience
 
-  # Choose what kind of storage to use for this uploader:
-  # storage :file
-  # storage :fog
-
-  # Override the directory where uploaded files will be stored.
-  # This is a sensible default for uploaders that are meant to be mounted:
-  #def store_dir
-    #"images/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-    #"#{Rails.root}/tmp/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  #end
-
-  # Provide a default URL as a default if there hasn't been a file uploaded:
-  # def default_url
-  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
-  # end
-
-  # Process files as they are uploaded:
-  # process :scale => [200, 300]
-  #
-  # def scale(width, height)
-  #   # do something
-  # end
-
-  # Create different versions of your uploaded files:
   version :mini do
     process :resize_to_fill => [50,50]
   end
@@ -41,16 +14,8 @@ class ImageUploader < CarrierWave::Uploader::Base
     process :resize_to_fit => [200, 200]
   end
 
-  # Add a white list of extensions which are allowed to be uploaded.
-  # For images you might use something like this:
   def extension_white_list
     %w(jpg jpeg gif png)
   end
-
-  # Override the filename of the uploaded files:
-  # Avoid using model.id or version_name here, see uploader/store.rb for details.
-  #def filename
-  #  "avatar.jpg" if original_filename
-  #end
 
 end
