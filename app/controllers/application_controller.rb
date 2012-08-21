f1f054447c1da1cfa15864a172a9d6a7ce01a69e
Dwayne Macgowan <dwaynemac@gmail.com>
2012-08-20 21:16:33 -0300

    decode arrays encoded by Typhoeus 0.5.0

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,24 @@
+require 'typhoeus/arrays_decoder'
+
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :decode_typhoeus_arrays
+
+  private
+
+  def decode_typhoeus_arrays
+    deep_decode(params)
+  end
+
+  # Recursively decode Typhoeus encoded arrays
+  def deep_decode(hash)
+    return hash unless hash.is_a?(Hash)
+    hash.each_pair do |key,value|
+      if value.is_a?(Hash)
+        deep_decode(value)
+        hash[key] = value.decode_typhoeus_array
+      end
+    end
+  end
 end
