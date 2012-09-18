7051e5782402217ebb251f7fd55f51c05a9a1584
sebareca <sebareca@gmail.com>
2012-09-17 21:42:23 -0300

    [#33782501] crm_merge logical model

--- config/initializers/logical_model.rb
+++ config/initializers/logical_model.rb
@@ -8,3 +8,12 @@ PADMA_ACCOUNTS_HOST = case Rails.env
   when "test"
     "localhost:3001"
 end
+
+PADMA_CRM_HOST = case Rails.env
+  when "production"
+    "padma-crm.heroku.com"
+  when "development"
+    "localhost:3000"
+  when "test"
+    "localhost:3000"
+end
---
+++ lib/crm_merge.rb
@@ -0,0 +1,14 @@
+# wrapper for CRM-Merge API interaction
+# Configuration for LogicalModel on /config/initializers/logical_model.rb
+class CrmMerge < LogicalModel
+
+  self.hydra = HYDRA
+  self.use_ssl = (Rails.env == "production")
+  self.resource_path = "/merges"
+  self.attribute_keys = [:parent_id, :son_id]
+  self.use_api_key = true
+  self.api_key_name = "token"
+  self.api_key = "8c330b5d70f86ebfa6497c901b299b79afc6d68c60df6df0bda0180d3777eb4a5528924ac96cf58a25e599b4110da3c4b690fa29263714ec6604b6cb2d943656"
+  self.host  = PADMA_CRM_HOST
+
+end
