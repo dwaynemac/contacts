8488b8e74f25038eb5c9b39275df02427bc1eded
Alex Falke <afalkear@gmail.com>
2012-10-12 21:07:46 -0300

    branch updated

--- spec/controllers/v0/avatars_controller_spec.rb
+++ spec/controllers/v0/avatars_controller_spec.rb
@@ -19,35 +19,35 @@ describe V0::AvatarsController do
     end
     context "if contact already has an avatar" do
       before do
-	@image = fixture_file_upload('spec/support/ghibli_main_logo.gif', 'image/gif')
-	@contact = Contact.make(:avatar => @image)
-	@new_image = fixture_file_upload('spec/support/robot3.jpg', 'image/jpg')
-	post  :create,
-	      :contact_id => @contact.id,
-	      :avatar => {:file => @new_image},
-	      :app_key => V0::ApplicationController::APP_KEY
-	end
-	it "should update the image as avatar" do
-	  @contact.reload
-	  @contact.avatar.should_not be_nil
-	  @contact.avatar.url.should match /robot3\.jpg/
-	end
+      	@image = fixture_file_upload('spec/support/ghibli_main_logo.gif', 'image/gif')
+      	@contact = Contact.make(:avatar => @image)
+      	@new_image = fixture_file_upload('spec/support/robot3.jpg', 'image/jpg')
+      	post  :create,
+      	      :contact_id => @contact.id,
+      	      :avatar => {:file => @new_image},
+      	      :app_key => V0::ApplicationController::APP_KEY
+      end
+    	it "should update the image as avatar" do
+    	  @contact.reload
+    	  @contact.avatar.should_not be_nil
+    	  @contact.avatar.url.should match /robot3\.jpg/
+    	end
     end
   end
   
   describe "#destroy" do
-	before do
-	  @image = fixture_file_upload('spec/support/ghibli_main_logo.gif', 'image/gif')
-	  @contact = Contact.make(:avatar => @image)
-	  delete  :destroy,
-	        :contact_id => @contact.id,
-	        :app_key => V0::ApplicationController::APP_KEY
-	end
-	it {should respond_with :success}
-	it "should remove avatar" do
-	  c = Contact.find(@contact.id)
-	  c.avatar.should be_blank
-	end
+  	before do
+  	  @image = fixture_file_upload('spec/support/ghibli_main_logo.gif', 'image/gif')
+  	  @contact = Contact.make(:avatar => @image)
+  	  delete  :destroy,
+  	        :contact_id => @contact.id,
+  	        :app_key => V0::ApplicationController::APP_KEY
+  	end
+  	it {should respond_with :success}
+  	it "should remove avatar" do
+  	  c = Contact.find(@contact.id)
+  	  c.avatar.should be_blank
+  	end
   end
 
   after(:each) do
