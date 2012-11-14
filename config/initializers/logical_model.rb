04b630d04d48703fe66338b168fb480090172e9e
Dwayne Macgowan <dwaynemac@gmail.com>
2012-11-14 20:41:46 -0300

    logical model staging logger

--- config/initializers/logical_model.rb
+++ config/initializers/logical_model.rb
@@ -26,3 +26,11 @@ module ActivityStream
   HYDRA = ::HYDRA
   API_KEY = "6d1a2dd931ef48d5f0c4d62de773825d3369ab426811c79c55e40569bc7bf044a437bbf569f765e6fd3a282ab43a27a2cb48ee2bd08c8bf743190165cd2ecb76"
 end
+
+class LogicalModel
+  if Rails.env.production? || Rails.env.staging?
+    def self.logger
+      Logger.new(STDOUT)
+    end
+  end
+end
