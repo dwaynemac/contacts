421b64f418238b25649090d98d58dda96d679f6b
Dwayne Macgowan <dwaynemac@gmail.com>
2012-12-10 18:22:07 -0300

    update accounts-ws url

--- config/initializers/logical_model.rb
+++ config/initializers/logical_model.rb
@@ -2,7 +2,7 @@ HYDRA = Typhoeus::Hydra.new
 
 PADMA_ACCOUNTS_HOST = case Rails.env
   when "production"
-    "padma-accounts.heroku.com"
+    "padma-accounts.herokuapp.com"
   when "staging"
     "padma-accounts-staging.herokuapp.com"
   when "development"
