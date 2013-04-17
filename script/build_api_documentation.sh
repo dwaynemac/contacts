bc4fe753318473eb74adeee2750793f5a3b8fe16
Dwayne Macgowan <dwaynemac@gmail.com>
2013-04-17 10:44:00 -0300

    change API Documentation title

--- script/build_api_documentation.sh
+++ script/build_api_documentation.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-bundle exec yardoc --title "My API Documentation" --plugin restful --readme API_README --output-dir ./public/documentation app/models/**/*.rb app/controllers/**/*.rb
+bundle exec yardoc --title "Contacts-ws Documentation" --plugin restful --readme API_README --output-dir ./public/documentation app/models/**/*.rb app/controllers/**/*.rb
 echo "Open public/documentation/index.html on your browser or browse to your-app-url/documentation"
