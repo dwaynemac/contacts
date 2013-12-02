e0dc35fe71b8a14c0421af1a4b7560f4f1895478
Dwayne Macgowan <dwaynemac@gmail.com>
2013-12-02 13:15:51 -0300

    [#61549474] Special flag for email duplication
    To allow an email to be duplicate Email#allow_duplicate must be true AND
    Contact#check_duplicates false
    Eg:
    
    c = Contact.new
    c.contact_attribues << Email.new(value: 'email@m.com',
                                     allow_duplicate: true)
    c.check_duplicates = false
    c.save

--- app/validators/email_uniqueness_validator.rb
+++ app/validators/email_uniqueness_validator.rb
@@ -1,7 +1,9 @@
 require 'mail'
 class EmailUniquenessValidator < ActiveModel::EachValidator
+  
   def validate_each(record,attribute,value)
-    return unless record.contact.present? && record.contact.check_duplicates
+    return if record.allow_duplicate
+    return unless record.contact.present?
     r = Contact.any_of({'_id' => {'$ne' =>record.contact._id}, 'contact_attributes._type' => 'Email', 'contact_attributes.value' => value})
 
     record.errors[attribute] << (options[:message] || "is not unique") if r.count > 0
--- spec/models/email_spec.rb
+++ spec/models/email_spec.rb
@@ -1,24 +1,60 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe Email do
-  it { should validate_presence_of :value }
-end
-
-describe Email do
-  it "should be unique" do
-    c = Contact.make(:first_name => "Bart", :last_name => "Simpson")
-    c.contact_attributes << Email.new(:category => :personal, :value => "bart@thesimpsons.com")
-    c.save!
-    c2 = Contact.new(:first_name => "El", :last_name => "Barto")
-    c2.contact_attributes << Email.new(:category => :personal, :value => "bart@thesimpsons.com")
-    assert !c2.valid?
+  before do
+    Contact.destroy_all
   end
 
-  it "should be normalized" do
-    c = Contact.make(:first_name => "Shinji", :last_name => "Ikari")
-    c.contact_attributes << Email.new(:value => "EVA_PILOT_01@GMAIL.COM")
-    c.save!
-    c.email.should == "eva_pilot_01@gmail.com"
+  it { should validate_presence_of :value }
+
+  describe "with default flag check_duplicates" do
+    it "should be unique" do
+      c = Contact.make(:first_name => "Bart", :last_name => "Simpson")
+      c.contact_attributes << Email.new(:category => :personal, :value => "bart@thesimpsons.com")
+      c.save!
+      c2 = Contact.new(:first_name => "El", :last_name => "Barto")
+      c2.contact_attributes << Email.new(:category => :personal, :value => "bart@thesimpsons.com")
+      assert !c2.valid?
+    end
+
+    it "should be normalized" do
+      c = Contact.make(:first_name => "Shinji", :last_name => "Ikari")
+      c.contact_attributes << Email.new(:value => "EVA_PILOT_01@GMAIL.COM")
+      c.save!
+      c.email.should == "eva_pilot_01@gmail.com"
+    end
+  end
+
+  describe "with flag check_duplicates false" do
+    it "should be unique" do
+      c = Contact.make(:first_name => "Bart", :last_name => "Simpson")
+      c.contact_attributes << Email.new(:category => :personal, :value => "bart@thesimpsons.com")
+      c.save!
+      c2 = Contact.new(:first_name => "El", :last_name => "Barto")
+      c2.contact_attributes << Email.new(:category => :personal, :value => "bart@thesimpsons.com")
+      c2.check_duplicates = false
+      c2.should_not be_valid
+    end
+
+    it "should be normalized" do
+      c = Contact.make(:first_name => "Shinji", :last_name => "Ikari")
+      c.contact_attributes << Email.new(:value => "EVA_PILOT_01@GMAIL.COM")
+      c.check_duplicates = false
+      c.save!
+      c.email.should == "eva_pilot_01@gmail.com"
+    end
+
+    describe "and flag allow_duplicate true" do
+      it "should NOT be unique" do
+        c = Contact.make(:first_name => "Bart", :last_name => "Simpson")
+        c.contact_attributes << Email.new(:category => :personal, :value => "bart@thesimpsons.com")
+        c.save!
+        c2 = Contact.new(:first_name => "El", :last_name => "Barto")
+        c2.contact_attributes << Email.new(:category => :personal, :value => "bart@thesimpsons.com", allow_duplicate: true)
+        c2.check_duplicates = false
+        c2.should be_valid
+      end
+    end
   end
 
 end
