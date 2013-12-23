e242b523398914968b005ed41584874908c6e6f2
Dwayne Macgowan <dwaynemac@gmail.com>
2013-12-23 14:04:48 -0300

    [#62910848] move credentials to application.yml
    - no need to diferentiate production / non-production this is managed by
      config file application.yml.

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -7,25 +7,14 @@ else
   CarrierWave.configure do |config|
     config.cache_dir = "#{Rails.root}/tmp/uploads"
     config.storage = :fog
-    if Rails.env.production?
-      config.fog_credentials = {
-          :provider               => 'AWS',       # required
-          :aws_access_key_id      => 'AKIAINBCXC7Y4PANEJOQ',       # required
-          :aws_secret_access_key  => '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',       # required
-          :region                 => 'sa-east-1'
-      }
-      config.fog_directory = 'contacts-prod'
-      config.fog_host = 'http://contacts-prod.s3-sa-east-1.amazonaws.com'
-    else
-      config.fog_credentials = {
-          :provider               => 'AWS',       # required
-          :aws_access_key_id      => 'AKIAINBCXC7Y4PANEJOQ',       # required
-          :aws_secret_access_key  => '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',       # required
-          #:region                 => 'eu-west-1'  # optional, defaults to 'us-east-1'
-      }
-      config.fog_directory  = 'contacts-dev'                     # required
-      config.fog_host       = "http://contacts-dev.s3.amazonaws.com"            # optional, defaults to nil
-    end
+    config.fog_credentials = {
+        :provider               => 'AWS',
+        :aws_access_key_id      => ENV['aws_access_key_id'],
+        :aws_secret_access_key  => ENV['aws_secret_access_key'],
+        :region                 => ENV['aws_region']
+    }
+    config.fog_directory  =        ENV['aws_bucket']
+    config.fog_host       =        ENV['aws_host']
 
     #config.fog_public     = false                                   # optional, defaults to true
     #config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
