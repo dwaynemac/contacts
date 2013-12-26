1dcdf4db5700d7af145b887ad4755b38343b374b
¨sebareca¨ <sebareca@gmail.com>
2013-12-26 16:15:03 -0300

    [#61159076] change api_key to the default (in CRM)

--- lib/crm_merge.rb
+++ lib/crm_merge.rb
@@ -10,7 +10,7 @@ class CrmMerge < LogicalModel
   self.attribute_keys = [:parent_id, :son_id]
   self.use_api_key = true
   self.api_key_name = "app_key"
-  self.api_key = "844d8c2d20"
+  self.api_key = "12341234124"
   self.host  = PADMA_CRM_HOST
 
   def json_root
