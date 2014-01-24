e9b93a4654aee17a089c4731cc29c8851b993083
Dwayne Macgowan <dwaynemac@gmail.com>
2014-01-24 18:29:03 -0300

    [#64466776]failing specs

--- spec/models/import_spec.rb
+++ spec/models/import_spec.rb
@@ -10,22 +10,22 @@ describe Import do
                 country_id state identity publish_on_gdp last_enrollment in_formation id_scan padma_id foto_migrated
                 id_scan_migrated padma_follow_id)
     @former_student = ["50001",	"30 366 832", "Dwayne",	"Macgowan", "Arribeños 2153 14B", "4783.6951", "15.4099.5071",
-                 "dwaynemac@gmail.com",	"asistente", "daniel.ferztand", "exalumno", "m",
+                 "dwaynemac@gmail.com",	"asistente", "daniel.ferztand", "exalumno", "male",
                  "/home/alex/workspace/Padma/public/persona/foto/50001/654da12b6a7905f62633eae7e76688c5.jpg",
                  "1983-05-21", "2005-05-13", "Instr. Método DeRose", "<p>alguna observacion</p>", "true", "5",
                  "1428", "1", "", "", "2011-02-19 18:04:12 -0300", "", "", "", "", "", "", "", "", "",
                  "false", "", "", "", "", "", "", ""]
     @student =  ["50010", "", "Alex", "Falke", "", "4782 1495",	"15 5466 7896",	"afalkear@gmail.com", "asistente",
-                 "lucia.gagliardini", "alumno", "m",
+                 "lucia.gagliardini", "alumno", "female",
                  "https://fbcdn-sphotos-c-a.akamaihd.net/hphotos-ak-frc1/249140_10150188276702336_1924524_n.jpg", "1983-03-11", "2004-12-01",
                  "Instructor del Método DeRose. Ingeniero informático.", "", "true", "5", "", "1",	"1667392", "",
                  "2013-01-11 14:03:29 -0300", "", "", "", "", "", "", "", "", "", "true", "", "", "", "", "", "", ""]
     @student2 =  ["50010", "", "Alex", "Falke", "", "4782 1395", "14 5466 7896", "afkear@gmail.com", "asistente",
-                 "lucia.gagliardini", "alumno", "m",
+                 "lucia.gagliardini", "alumno", "female",
                  "https://fbcdn-sphotos-c-a.akamaihd.net/hphotos-ak-frc1/249140_10150188276702336_1924524_n.jpg", "1983-03-11", "2004-12-01",
                  "Instructor del Método DeRose. Ingeniero informático.", "", "true", "5", "", "1",  "1667392", "",
                  "2013-01-11 14:03:29 -0300", "", "", "", "", "", "", "", "", "", "true", "", "", "", "", "", "", ""]
-    @p_visit = ["50178", "", "Daniel", "Werber", "", "", "15 4437-6580", "werber@fibertel.com.ar", "aspirante", "",	"perfil", "m",
+    @p_visit = ["50178", "", "Daniel", "Werber", "", "", "15 4437-6580", "werber@fibertel.com.ar", "aspirante", "",	"perfil", "female",
                 "","","", "BioquÃ­mico", "", "false", "", "", "1", "", "", "", "", "", "", "", "", "", "", "", "", "false", 
                 "", "", "", "", "", "", ""]
     CSV.open("#{Rails.root}/spec/support/test.csv", "w") do |csv|
@@ -80,6 +80,11 @@ describe Import do
           dwayne = Contact.where(first_name: "Dwayne").first
           dwayne.local_unique_attributes.where(_type: "Observation").count.should == 1
           dwayne.local_unique_attributes.where(_type: "Observation").first.value.should == "<p>alguna observacion</p>"
+        end
+        it "should set gender correctly" do
+          @new_import.process_CSV_without_delay
+          dwayne = Contact.where(first_name: "Dwayne").first
+          dwayne.gender.should == 'male'
         end
       end
       context "with a contact that is already in the database" do
