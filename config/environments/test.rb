ad72cda95c31a7c3140e264f3a4252dded1b08b3
Dwayne Macgowan <dwaynemac@gmail.com>
2014-03-16 21:39:44 -0300

    fix bday notificator spec

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -5,7 +5,6 @@ Contacts::Application.configure do
   # test suite.  You never need to work with it otherwise.  Remember that
   # your test database is "scratch space" for the test suite and is wiped
   # and recreated between test runs.  Don't rely on the data there!
-  # avoid caching to avoid needed to reload spork on every change
   config.cache_classes = false
 
   # Configure static asset server for tests with Cache-Control for performance
