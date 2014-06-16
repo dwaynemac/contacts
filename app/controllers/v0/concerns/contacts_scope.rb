a23b2adf3ea596db9501c211d0d20422ad04fbc6
Dwayne Macgowan <dwaynemac@gmail.com>
2014-06-16 01:51:49 -0300

    add instruments to ContactsScope for analysis

--- app/controllers/v0/concerns/contacts_scope.rb
+++ app/controllers/v0/concerns/contacts_scope.rb
@@ -26,29 +26,37 @@ module V0
 
       # @optional [Array<Hash>] attributes_value_at Array of hashes with keys: :attribute, :value, :ref_date. This will be ANDed, not ORed.
       def refine_by_attributes_at_given_time
-        params[:attribute_values_at].each do |ava|
-          ava['value'] = ava['value'] == 'true' if ava['attribute'] == 'in_professional_training'
-          @scope = @scope.with_attribute_value_at(ava['attribute'],ava['value'],ava['ref_date'])
-        end if params[:attribute_values_at]
+        ActiveSupport::Notifications.instrument('attribute_at_given_time.refine_scope.contacts_search') do
+          params[:attribute_values_at].each do |ava|
+            ava['value'] = ava['value'] == 'true' if ava['attribute'] == 'in_professional_training'
+            @scope = @scope.with_attribute_value_at(ava['attribute'],ava['value'],ava['ref_date'])
+          end if params[:attribute_values_at]
+        end
       end
 
       # @optional [Array] nids return contacts without id in this array
       # @optional [Array] ids return contacts with id in this array
       def refine_by_ids
-        @scope = @scope.not_in(_id: params[:nids]) if params[:nids]
-        @scope = @scope.any_in(_id: params[:ids]) if params[:ids]
+        ActiveSupport::Notifications.instrument('ids.refine_scope.contacts_search') do
+          @scope = @scope.not_in(_id: params[:nids]) if params[:nids]
+          @scope = @scope.any_in(_id: params[:ids]) if params[:ids]
+        end
       end
 
       # @optional [String] full_text will make a full_text search with this string.
       def refine_by_full_text_search
-        @scope = @scope.csearch(params[:full_text]) if params[:full_text].present?
+        ActiveSupport::Notifications.instrument('full_text.refine_scope.contacts_search') do
+          @scope = @scope.csearch(params[:full_text]) if params[:full_text].present?
+        end
       end
 
       # @optional [Hash] where Mongoid where selector with additional keys -> :email, :telephone, :address, :local_status, :date_attribute
       def refine_with_contacts_searcher
-        if params[:where].present?
-          searcher = ContactSearcher.new(@scope, @account.try(:id))
-          @scope = searcher.api_where(params[:where])
+        ActiveSupport::Notifications.instrument('contacts_searcher.refine_scope.contacts_search') do
+          if params[:where].present?
+            searcher = ContactSearcher.new(@scope, @account.try(:id))
+            @scope = searcher.api_where(params[:where])
+          end
         end
       end
 
