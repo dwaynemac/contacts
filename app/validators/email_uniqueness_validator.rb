688f19c05e0506fd6691537c3db9d790959885fa
Dwayne Macgowan <dwaynemac@gmail.com>
2015-01-11 18:59:15 -0300

    [#85783978] use elemMatch for better performance and results

--- app/validators/email_uniqueness_validator.rb
+++ app/validators/email_uniqueness_validator.rb
@@ -4,7 +4,13 @@ class EmailUniquenessValidator < ActiveModel::EachValidator
   def validate_each(record,attribute,value)
     return if record.allow_duplicate
     return unless record.contact.present?
-    r = Contact.any_of({'_id' => {'$ne' =>record.contact._id}, 'contact_attributes._type' => 'Email', 'contact_attributes.value' => value})
+    r = Contact.any_of({
+      '_id' => {'$ne' =>record.contact._id},
+      contact_attributes: { '$elemMatch' => {
+        '_type' => 'Email',
+        value: value
+        } 
+    }})
 
     record.errors[attribute] << (options[:message] || "is not unique") if r.count > 0
   end
