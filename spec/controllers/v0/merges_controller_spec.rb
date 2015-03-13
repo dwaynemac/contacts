4ca844eb914dd977535daa6398b48777ab008c2d
Dwayne Macgowan <dwaynemac@gmail.com>
2015-03-13 10:53:56 -0300

    [#80872114] fix controller spec

--- spec/controllers/v0/merges_controller_spec.rb
+++ spec/controllers/v0/merges_controller_spec.rb
@@ -97,6 +97,7 @@ describe V0::MergesController do
           ActivityStream::Activity.any_instance.stub(:create).and_return(true)
           ActivityStream::Merge.any_instance.stub(:create).and_return(true)
           PlanningMerge.any_instance.stub(:create).and_return(true)
+          FnzMerge.any_instance.stub(:create).and_return(true)
 
 
           @post_args = {account_name: @acc.name,
