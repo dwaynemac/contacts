6322628581eaee8ae529934a0b42b9f1bcb34972
Dwayne Macgowan <dwaynemac@gmail.com>
2015-03-29 17:19:30 -0300

    move worker into same ec2 instance as webserver

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,19 +7,24 @@ set :rails_env, 'production'
 set :bundle_without, 'development:test:heroku:deploying'
 
 set :web_host, 'contacts.padm.am'
-set :worker_host, '54.211.11.34'
+# set :worker_host, '54.211.11.34'
+
 set :user, 'ec2-user'
 set :key_path, "#{File.dirname(File.realpath(__FILE__))}/production.pem"
+
+role :web, fetch(:web_host)
+role :worker, fetch(:web_host)
 
 server fetch(:web_host),
        name: 'contacts-app-capified',
        user: fetch(:user),
-       roles: %w(web app),
+       roles: %w(web app worker),
        ssh_options: {
          keys: [fetch(:key_path)],
          auth_methods: %w(publickey)
        }
 
+=begin
 server fetch(:worker_host),
        name: 'contacts-worker',
        user: fetch(:user),
@@ -28,3 +33,4 @@ server fetch(:worker_host),
          keys: [fetch(:key_path)],
          auth_methods: %w(publickey)
        }
+=end
