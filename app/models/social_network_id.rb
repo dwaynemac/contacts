e0a5cc99418698c8e4e170a55406821b296496a3
Dwayne Macgowan <dwaynemac@gmail.com>
2015-04-15 18:22:23 -0300

    [#88372822] Social Network Id no longer unique

--- app/models/social_network_id.rb
+++ app/models/social_network_id.rb
@@ -2,30 +2,10 @@ class SocialNetworkId < ContactAttribute
   field :category
   field :value
 
-  validates :value, :presence => true, :uniqueness => {:scope => :category}, :unless => :check_duplicates
-  validates :category, :presence => true, :uniqueness => {:scope => :contact}, :unless => :check_duplicates
-
-  before_validation :check_value_uniqueness
-
-  def check_value_uniqueness
-    return if _parent.nil? || !_parent.check_duplicates
-    r = Contact.where({'_id' => {'$ne' =>contact._id}})
-              .and( contact_attributes: { '$elemMatch' => {
-                      value: value,
-                      category: category }})
-    if r.count > 0
-      errors[:value] << I18n.t('errors.messages.is_not_unique')
-      contact.errors[:possible_duplicates] << r.map {|c| c.minimum_representation}
-    end
-  end
+  validates :value, :presence => true
+  validates :category, :presence => true
 
   def get_normalized_value
     self.value.gsub(/[\.\-_\s\/]/,'')
-  end
-
-  private
-
-  def check_duplicates
-    _parent.check_duplicates
   end
 end
