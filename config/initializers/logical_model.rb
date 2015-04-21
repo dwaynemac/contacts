766cba483b2b4131dc12add143eede67eff8b950
Sebastian Reca <sebareca@gmail.com>
2015-04-21 09:56:14 -0300

    [#86743286] Move keys from initializers/logical_model.rb to application.yml.example
    Also change to secret

--- config/initializers/logical_model.rb
+++ config/initializers/logical_model.rb
@@ -45,13 +45,13 @@ PADMA_CRM_HOST = case Rails.env
 end
 
 module Accounts
-  API_KEY = "8c330b5d70f86ebfa6497c901b299b79afc6d68c60df6df0bda0180d3777eb4a5528924ac96cf58a25e599b4110da3c4b690fa29263714ec6604b6cb2d943656"
+  API_KEY = ENV['accounts_key']
   HYDRA = ::HYDRA
 end
 
 module ActivityStream
   HYDRA = ::HYDRA
-  API_KEY = "6d1a2dd931ef48d5f0c4d62de773825d3369ab426811c79c55e40569bc7bf044a437bbf569f765e6fd3a282ab43a27a2cb48ee2bd08c8bf743190165cd2ecb76"
+  API_KEY = ENV['activity_key']
 end
 
 module Messaging
@@ -69,5 +69,5 @@ end
 
 module Overmind
   HYDRA = ::HYDRA
-  API_KEY = "secret-key"
+  API_KEY = ENV['overmind_key']
 end
