18ed424e55b27946f4b9864a0bc9bf2f55b05a12
Alex Falke <afalkear@gmail.com>
2015-09-06 15:18:14 -0300

    [#61928400] search professions in given address

--- app/controllers/v0/occupations_controller.rb
+++ app/controllers/v0/occupations_controller.rb
@@ -8,7 +8,11 @@ class V0::OccupationsController < V0::ApplicationController
   # @url /v0/occupations
   # @action GET
   #
-  # @optional [String] status only contacts with choosen status
+  # @optional [String] status only contacts with chosen status
+  # @optional [String] country only contacts with chosen country
+  # @optional [String] state only contacts with chosen state
+  # @optional [String] city only contacts with chosen city
+  # @optional [String] neighborhood only contacts with chosen neighborhood
   # @optional [Bool] only_with_address only contacts wich have an address
   #
   # @response_field [Array <String>] occupations
@@ -23,15 +27,20 @@ class V0::OccupationsController < V0::ApplicationController
     if params[:only_with_address] == "true"
       query['$and'] = [
         {"contact_attributes._type" => "Occupation"},
-        {"contact_attributes._type" => "Address"} 
+        {"contact_attributes._type" => "Address"}
       ]
+      %w(country state city neighborhood).each do |a|
+        if params[a.to_sym]
+          query['$and'] << {"contact_attributes.#{a}" => params[a.to_sym]}
+        end
+      end
     else
       query["contact_attributes._type"] = "Occupation"
     end
 
     occupations = []
     Contact.where(query).each do |contact|
-      occupations = occupations | contact.occupations.map {|occ| occ.value}
+      occupations = occupations | contact.occupations.map {|occ| occ.value} 
     end
     
     render :json => {:occupations => occupations}.to_json, :status => 200
