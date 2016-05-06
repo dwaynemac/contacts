c2ff1592d3b82d1ca14874466704ecb9caf6d803
Alex Falke <afalkear@gmail.com>
2016-05-06 10:46:22 -0300

    [#118511159] add mailer to contacts

---
+++ app/mailers/contacts_mailer.rb
@@ -0,0 +1,3 @@
+class ContactsMailer < ActionMailer::Base
+  default from: "from@example.com"
+end
---
+++ spec/mailers/contacts_mailer_spec.rb
@@ -0,0 +1,5 @@
+require "spec_helper"
+
+describe ContactsMailer do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
