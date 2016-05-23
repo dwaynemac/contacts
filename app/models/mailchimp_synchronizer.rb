5ebeb682625ffae8de4273917c25d542604922bb
Dwayne Macgowan <dwaynemac@gmail.com>
2016-05-23 19:00:08 -0300

    remove sleep

--- app/models/mailchimp_synchronizer.rb
+++ app/models/mailchimp_synchronizer.rb
@@ -44,7 +44,7 @@ class MailchimpSynchronizer
         Rails.logger.info "[mailchimp_synchronizer #{self.id}] retrying: #{e.message}"
         retries -= 1
         if retries >= 0
-          sleep((RETRIES-retries)*10)
+          # sleep((RETRIES-retries)*10)
           retry
         else
           Rails.logger.info "[mailchimp_synchronizer #{self.id}] failed: #{e.message}"
