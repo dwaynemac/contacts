47eac07762cb0d371d074db2793a368a1d041713
Dwayne Macgowan <dwaynemac@gmail.com>
2021-04-27 16:00:50 -0300

    use aws-ses signature v4

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,3 +1,4 @@
 ActionMailer::Base.add_delivery_method :ses, AWS::SES::Base,
   :access_key_id     => ENV['padma_aws_key_id'],
-  :secret_access_key => ENV['padma_aws_secret_access_key']
+  :secret_access_key => ENV['padma_aws_secret_access_key'],
+  :signature_version => 4
